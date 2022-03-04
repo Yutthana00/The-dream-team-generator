f0d259342026315e61ac356dc6bbc85e0dc94042
Yutthana00 <arf101293@gmail.com>
2022-03-04 12:54:07 +0000

    First push, npm init installed, index js created

---
+++ index.js
@@ -0,0 +1,10 @@
+//node modules
+
+const inquruer = require("inquirer");
+const fs = require("fs");
+const generatTeam = require("");
+
+//lib modules
+const Engineer = require("")
+const Intern = require("")
+const Manager = require("")
