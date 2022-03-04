c0fb7007c1414428ebbe2d6f19c7783935d16151
Yutthana00 <arf101293@gmail.com>
2022-03-04 13:24:45 +0000

    Finished question function

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const newStaffMemberData = [];
 
 // array of object questions asked in CLI to users
 const questions = async () => {
-    const anwers = await inquirer
+    const answers = await inquirer
     .prompt([
         {
             type:"input",
@@ -40,5 +40,61 @@ const questions = async () => {
         },
     ])
 
+// log answers to corresponding question:
+    //If manager is selected:
+    if(answers.role === "Manager") {
+        const managerAns = await inquirer
+            .prompt([
+                {
+                    type:"input",
+                    message:"What is your office number?",
+                    name:"office number",
+                }
+            ])
+            const newManager = new Manager(
+                answers.name,
+                answers.id,
+                answers.email,
+                managerAns.officeNumber
+            );
+            newStaffMemberData.push(newManager);
+
+    //If Engineer is selected:
+    } else if (answers.role === "Engineer") {
+        const githubAns = await inquirer
+        .prompt([
+            {
+                type:"input",
+                messages:"What is your Github UserName?",
+                name:"github", 
+            }
+        ])
+        const newEngineer = new Engineer(
+            answers.name,
+            answers.id,
+            answers.email,
+            answers.github
+        );
+        newStaffMemberData.push(newEngineer);
     
-}
+     //If Intern is selected:
+    }else if (answers.role === "Intern") {
+        const internAns = await inquirer
+        .prompt([
+            {
+                type:"input",
+                message:"What university did you attend?",
+                name:"university",
+            },
+        ])
+
+        const newIntern = new Intern(
+            answers.name,
+            answers.id,
+            answers.email,
+            internAns.university
+        );
+        newStaffMemberData.push(newIntern);
+    }
+
+}; //End of question function.
