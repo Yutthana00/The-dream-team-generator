6626193f98698144e6d0c9b5d0c47b06a9bbf25b
Yutthana00 <arf101293@gmail.com>
2022-03-04 13:03:59 +0000

    index input created

--- index.js
+++ index.js
@@ -7,4 +7,38 @@ const generatTeam = require("");
 //lib modules
 const Engineer = require("")
 const Intern = require("")
-const Manager = require("")
+const Manager = require("");
+const inquirer = require("inquirer");
+
+// Array for answers to questions
+const newStaffMemberData = [];
+
+// array of object questions asked in CLI to users
+const questions = async () => {
+    const anwers = await inquirer
+    .prompt([
+        {
+            type:"input",
+            message:"What is your name?",
+            name:"name",
+        },
+        {
+            type:"input",
+            message:"What is your ID",
+            name:"id",
+        },
+        {
+            type:"input",
+            message:"What is your email",
+            name:"email",
+        },
+        {
+            type:"list",
+            message:"What is your role? ",
+            name:"role",
+            choices:["Engineer", "Intern", "Manger"], 
+        },
+    ])
+
+    
+}
