a44f03209bac9e1e20305350c5e97ed6117cf7ef
Yutthana00 <arf101293@gmail.com>
2022-04-29 03:38:50 +0100

    title change

--- src/page-template.js
+++ src/page-template.js
@@ -21,7 +21,7 @@ const generateTeam = (team) => {
     };
     
     // create the manager html
-const generateManager = (manager) => {
+    const generateManager = (manager) => {
         return `
         <div class="card employee-card">
         <div class="card-header">
@@ -90,7 +90,7 @@ module.exports = (team) => {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>My Team</title>
+    <title>My Aswesome Dream Team</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
@@ -101,7 +101,7 @@ module.exports = (team) => {
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 jumbotron mb-3 team-heading">
-                <h1 class="text-center">My Team</h1>
+                <h1 class="text-center">My Awesome Dream Team</h1>
             </div>
         </div>
     </div>
